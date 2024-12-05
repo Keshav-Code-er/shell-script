29c2cbb3966e8a4b28ef507ae1e3991a4dafcc5c
KeshavK <keshavsign8@gmail.com>
2024-12-05 14:32:10 +0530

    Second Changes Script added

--- 02-variable.sh
+++ 02-variable.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 PERSON1=$1 #this is for first value from command line
-PERSON2=&2 #this is second value from command line
+PERSON2=$2 #this is second value from command line
 echo "Hello World"
 echo "$PERSON1: Good Morning"
 echo "$PERSON2: Yeah! Good Morning, How are you"
