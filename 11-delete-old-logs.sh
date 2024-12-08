568e69c52690d13196c3b91a64280f8249c40821
KeshavK <keshavsign8@gmail.com>
2024-12-08 20:04:57 +0530

    Second Changes Script added

--- 11-delete-old-logs.sh
+++ 11-delete-old-logs.sh
@@ -2,7 +2,7 @@
 
 APP_LOGS_DIR=/home/centos/app-log
 
-DATE=$(date +%F)
+DATE=$(date +%F: %H:%M:%S)
 LOGDIR=/home/centos/shell-script/shell_script-log
 ##/home/centos/shell-script/shell_script-log/script-name-date.log
 SCRIPT_NAME=$0
@@ -10,7 +10,7 @@ LOGFILE=$LOGDIR/$0-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
-echo "$FILES_TO_DELETE"
+echo "Script started executing at $DATE"
 
 while read line; do
     echo "Deleting $line" &>>$LOGFILE
